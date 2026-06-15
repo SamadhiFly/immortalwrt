3be374ed9c1bc6d0a961308bf9897b8362439012
SamadhiFly <lsgx@live.com>
2026-06-15 15:50:47 +0800

    Change default config for imageName hostName wifiName banner firewall uhttpd
    Signed-off-by: SamadhiFly <lsgx@live.com>

--- include/version.mk
+++ include/version.mk
@@ -27,7 +27,10 @@ PKG_CONFIG_DEPENDS += \
 sanitize = $(call tolower,$(subst _,-,$(subst $(space),-,$(1))))
 
 VERSION_NUMBER:=$(call qstrip,$(CONFIG_VERSION_NUMBER))
-VERSION_NUMBER:=$(if $(VERSION_NUMBER),$(VERSION_NUMBER),25.12.0)
+VERSION_NUMBER:=$(if $(VERSION_NUMBER),$(VERSION_NUMBER),$(shell cat $(TOPDIR)/.vernum))
+
+VERSION_COMMIT:=$(shell git rev-parse --short HEAD)
+VERSION_COMMIT_DATE:=$(shell git show -s --format=%ci $(VERSION_COMMIT))
 
 VERSION_CODE:=$(call qstrip,$(CONFIG_VERSION_CODE))
 VERSION_CODE:=$(if $(VERSION_CODE),$(VERSION_CODE),r37854-4b24da3b4c5c)
@@ -36,11 +39,11 @@ VERSION_REPO:=$(call qstrip,$(CONFIG_VERSION_REPO))
 VERSION_REPO:=$(if $(VERSION_REPO),$(VERSION_REPO),https://downloads.immortalwrt.org/releases/25.12.0)
 
 VERSION_DIST:=$(call qstrip,$(CONFIG_VERSION_DIST))
-VERSION_DIST:=$(if $(VERSION_DIST),$(VERSION_DIST),ImmortalWrt)
+VERSION_DIST:=$(if $(VERSION_DIST),$(VERSION_DIST),LWrt)
 VERSION_DIST_SANITIZED:=$(call sanitize,$(VERSION_DIST))
 
 VERSION_MANUFACTURER:=$(call qstrip,$(CONFIG_VERSION_MANUFACTURER))
-VERSION_MANUFACTURER:=$(if $(VERSION_MANUFACTURER),$(VERSION_MANUFACTURER),ImmortalWrt)
+VERSION_MANUFACTURER:=$(if $(VERSION_MANUFACTURER),$(VERSION_MANUFACTURER),LWrt)
 
 VERSION_MANUFACTURER_URL:=$(call qstrip,$(CONFIG_VERSION_MANUFACTURER_URL))
 VERSION_MANUFACTURER_URL:=$(if $(VERSION_MANUFACTURER_URL),$(VERSION_MANUFACTURER_URL),https://immortalwrt.org/)
@@ -113,4 +116,6 @@ VERSION_SED_SCRIPT:=$(SED) 's,%U,$(call sed_escape,$(VERSION_REPO)),g' \
 	-e 's,%f,$(call sed_escape,$(VERSION_FIRMWARE_URL)),g' \
 	-e 's,%P,$(call sed_escape,$(VERSION_PRODUCT)),g' \
 	-e 's,%h,$(call sed_escape,$(VERSION_HWREV)),g' \
-	-e 's,%B,$(call sed_escape,$(SOURCE_DATE_EPOCH)),g'
+	-e 's,%B,$(call sed_escape,$(SOURCE_DATE_EPOCH)),g' \
+	-e 's,%a,$(call sed_escape,$(VERSION_COMMIT)),g' \
+	-e 's,%x,$(call sed_escape,$(VERSION_COMMIT_DATE)),g'
